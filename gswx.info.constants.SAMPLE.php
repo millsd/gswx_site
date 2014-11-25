aa56e65f7b3df4e801c82e990d17198e50ee1cbd
millsd <git@qorg.org>
2014-11-25 09:49:43 -0800

    move API keep from httpdocs/ to gswx.info.constants.php

---
+++ gswx.info.constants.SAMPLE.php
@@ -0,0 +1,13 @@
+<?php
+
+define('GSWX_PARENT_DIR', 'the absolute path to directory containing gswx.info.constants.php');
+define('GSWX_APPROOT', 'e.g.: ' . GSWX_PARENT_DIR . 'gswx_site/');
+define('GSWX_DATAROOT', 'e.g.: ' . GSWX_PARENT_DIR . 'gswx_data/');
+define('ENVIRONMENT', 'development');
+define('DB_HOSTNAME', 'db host');
+define('DB_USERNAME', 'db user');
+define('DB_PASSWORD', 'db pw');
+define('DB_DATABASE', 'db name');
+define('GOOGLE_API_KEY', 'your google maps key');
+
+/* End of file */
